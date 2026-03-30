2203dfd7c42a9527ae558f2b290c5c0cb77eb9ba
jodumu <brucenic12@gmail.com>
2026-03-30 02:11:26 +0100

    hackathon

--- hagathon-day1.go
+++ hagathon-day1.go
@@ -1,51 +1,88 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
-	var oper int
-	var cal1 int
-	var cal2 int
+
+	blue := "\033[34m"
+	red := "\033[31m"
+	green := "\033[32m"
+	yellow := "\033[33m"
+	reset := "\033[0m"
+
+	var oper string
+	var a string
+	var b string
+	var Help string
 
 	for {
-        start:
-		fmt.Println("Enter Fist no: ")
-		_, err1 := fmt.Scan(&cal1)
+		
+		fmt.Println(green + "Welcome to my smart calculator" + reset)
+		fmt.Println(green + "If you are having any trouble please type help for more assitance" + reset)
+		fmt.Println(green + "If you don't have any type cont to continue" + reset)
+		fmt.Println(green + "And if don't want to continue operation type quit" + reset)
+		fmt.Scan(&Help)
 
+		start:
+		if Help == "help" {
+			fmt.Println(yellow + "First enter a number" + reset)
+			fmt.Println("Then from the option 1-5 for any operation")
+			goto start2
+		} else if Help == "quit" {
+			fmt.Println(green + "===Thanks for your time===" + reset)
+			break
+		} else if Help == "cont" {
+			fmt.Println(yellow + "Let start calculating" + reset)
+			goto start2
+		} else {
+			fmt.Println(red + "This is not an operation choose a proper operation" + reset)
+			goto start
+		}
 
-		fmt.Println("Enter second no: ")
-        fmt.Scan(&cal2)
+		start2:
+		fmt.Println(blue + "Enter Fist no: " + reset)
+		fmt.Scan(&a)
 
-        if err1 != nil  {
-            fmt.Println("Input a valid number:")
+		fmt.Println(blue + "Enter second no: " + reset)
+        fmt.Scan(&b)
+		a1 , err1 := strconv.Atoi(a)
+		b1 , err2 := strconv.Atoi(b)
+
+        if err1 != nil || err2 != nil {
+            fmt.Println(red + "Input A Valid Number " + reset)
+			fmt.Println(red + "Try Again" + reset)
             goto start
-        } else {
-            continue
         }
 
-		fmt.Println("Which operation would you like Add[1], Sub[2], Mul[3], Div[4], Quit[5]: ")
+		if a == "quit" || b == "quit" {
+			fmt.Println(green + "===Thanks for your time===" + reset)
+			break
+		}
+
+		fmt.Println(blue + "Which operation would you like Add[1], Sub[2], Mul[3], Div[4]: " + reset)
 		fmt.Scan(&oper)
 
-		a := cal1
-		b := cal2
-
-		if oper == 1 {
-			fmt.Println("Adding: ", fmt.Sprint(a+b))
-		} else if oper == 2 {
-			fmt.Println("Subtracting:", fmt.Sprint(a-b))
-		} else if oper == 3 {
-			fmt.Println("Multiplying:", fmt.Sprint(a*b))
-		} else if oper == 4 {
-			if b == 0 {
-				fmt.Println("CAN'T do any Divition")
-				continue
+		if oper == "1" {
+			fmt.Println(green + "Added:" + reset, fmt.Sprint(a1+b1))
+		} else if oper == "2" {
+			fmt.Println(green + "Subtracted:" + reset, fmt.Sprint(a1-b1))
+		} else if oper == "3" {
+			fmt.Println(green + "Multiplied:" + reset, fmt.Sprint(a1*b1))
+		} else if oper == "4" {
+			if b1 == 0 {
+				fmt.Println(red + "CAN'T be divided by 0" + reset)
+				fmt.Println(red + "Lets start again" + reset)
+				goto start2
 			}
-			fmt.Println("Dividing:", fmt.Sprint(a/b))
-		} else if oper == 5 {
-			fmt.Println("===Thanks for your time===")
+			fmt.Println(green + "Divided:" + reset, fmt.Sprint(a1/b1))
+		} else if oper == "quit" {
+			fmt.Println(green + "===Thanks for your time===" + reset)
 			break
 		} else {
-			fmt.Println("Help: Not a operation choose from 1-5 for any disired operation")
+			goto start
 		}
 	}
 }
--- hagathon-day2.go
+++ hagathon-day2.go
@@ -1,45 +1,71 @@
 package main
 
 import (
-	"fmt"
 	"strconv"
+	"strings"
+	"bufio"
+	"fmt"
+	"os"
 )
    
 func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	blue := "\033[34m"
+	red := "\033[31m"
+	green := "\033[32m"
+	yellow := "\033[33m"
+	reset := "\033[0m"
 
 	for {
-	var conver string
-	fmt.Println("Enter convertion")
-	fmt.Print("> ")
-	fmt.Scan(&conver)
 
-	if conver == "" {
+		fmt.Print(green + "Enter convertion " + reset)
+
+		fmt.Print(yellow + "> " + reset)
+        if !scanner.Scan() {
+            break
+        }
+	
+	input := strings.TrimSpace(scanner.Text())
+	if input == "" {
 		continue
 	}
 
-	if conver == "quit" {
+	if input == "quit" {
 		break
 	}
 
-	if len(conver) != 3 || conver[0] != "conver" {
-		fmt.Println("Invalid operation use convert value, base")
+	conv := strings.Fields(input)
+	if len(conv) != 3 || conv[0] != "convert" {
+		fmt.Println(red + "Invalid operation use convert value, base" + reset)
 		continue
 	}
 	  
-	value := conver[1]
-	base := conver[2]
+	value := conv[1]
+	Base := conv[2]
 
-	num, err := strconv.ParseInt(value, base, 64)
+	var base int
+	switch Base {
+	case  "hex":
+		base = 16
+	case "bin":
+    	base = 2
+    case "dec":
+        base = 10
+	}
+
+	no, err := strconv.ParseInt(value, base, 64)
 	if err != nil {
-		fmt.Println("Invalid base number: ", base)
+		fmt.Println(red + "Invalid base number: " + reset, Base)
 		continue
 	}
 
-	var base int
-	switch base {
-	case  "hex"
-
-		fmt.Println()
-	}
+        switch Base {
+        case "dec":
+            fmt.Println(blue + "+ Binary: " + reset, strconv.FormatInt(no, 2))
+            fmt.Println(blue + "+ Hex: " + reset, strings.ToUpper(strconv.FormatInt(no, 16)))
+        default:
+            fmt.Println(blue + "+ Decimal:" + reset, no)
+        }
 	}
 }
